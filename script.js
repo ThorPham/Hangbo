c2e9ef3643f2a3d1a29b20dcce0ce20fe27da618
Pham Dung <dungphamvan0803@gmail.com>
2023-02-16 01:38:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,15 @@ $(window).on('mousemove click', function(e) {
 
 });
 
+
+$(window).on('touchmove', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (12 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+
 animate();
